989d36c2d53ebcdbaf058969b6725113defe3ecb
Martins Zariņš <hello@zarins.zip>
2026-04-30 16:56:03 +0300

    Tweaked some things.

--- Kods.py
+++ Kods.py
@@ -6,17 +6,18 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
-def read(path):
-	img = cv2.imread(path)
+def read(title):
+	img = cv2.imread(f'Bildes/{ title }.jpg')
 	return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
-def plot(img, title, save=os.getenv("SCRIPT_SAVE_IMG") is not None):
+def plot(img, title, save=os.getenv('SCRIPT_SAVE_IMG') is not None):
 	if save:
 		img = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
-		cv2.imwrite(f"Figūras/{ title }.jpg", img)
+		cv2.imwrite(f'Figūras/{ title }.jpg', img)
 		return
 
-	plt.imshow(img, cmap="gray")
+	plt.imshow(img, cmap='gray')
+
 	plt.title(title)
 	plt.axis(False)
 
