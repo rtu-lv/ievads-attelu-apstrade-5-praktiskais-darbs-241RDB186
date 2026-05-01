a0c1c8644a9a9ee8a64f0f9c6dc22ff63917e23e
Martins Zariņš <hello@zarins.zip>
2026-05-01 14:44:15 +0300

    Implemented gaussian_thresh. Use k_means with multiple k values.
    The previous method was not the one specified in the assignment...

--- Kods.py
+++ Kods.py
@@ -6,6 +6,8 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 
+plt.rcParams['image.cmap'] = 'summer'
+
 def read(path):
 	img = cv2.imread(path)
 	return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -24,76 +26,68 @@ def plot(img, title, save=os.getenv('SCRIPT_SAVE_IMG') is not None):
 	plt.show()
 
 
-def sort_labels(labels, centers):
-	intens = np.mean(centers, axis=1)
-	sorted = np.argsort(intens)
+def gaussian_thresh(img, kernel=15, c=0):
+	img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-	table = np.empty(len(intens), dtype=labels.dtype)
-	table[sorted] = np.arange(len(intens))
+	method = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+	thresh = cv2.THRESH_BINARY
 
-	return table[labels]
+	return cv2.adaptiveThreshold(img, 255, method, thresh, kernel, c)
 
-def draw_clusters(labels, clusters, cmap=plt.get_cmap('jet')):
-	output = np.empty((*labels.shape, 3), dtype=np.uint8)
-	for i in range(clusters):
-		rgb = cmap(i / clusters)[:3]
-		output[labels == i] = [np.uint8(c * 255) for c in rgb]
-
-	return output
-
-
-def gaussian_mixture(img, clusters=5, iter=20, eps=1):
-	data = img.reshape(-1, 3).astype(np.float32)
-
-	criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, iter, eps)
-
-	em = cv2.ml.EM.create()
-	em.setClustersNumber(clusters)
-	em.setTermCriteria(criteria)
-
-	_, _, labels, _ = em.trainEM(data)
-	centers = em.getMeans()
-
-	labels = labels.reshape(img.shape[:2])
-	labels = sort_labels(labels, centers)
-
-	return draw_clusters(labels, clusters)
-
-def k_means(img, clusters=5, iter=20, eps=1):
+def k_means(img, k=5, iter=20, eps=1):
 	data = img.reshape(-1, 3).astype(np.float32)
 
 	criteria = (cv2.TERM_CRITERIA_MAX_ITER + cv2.TERM_CRITERIA_EPS, iter, eps)
 	origin = cv2.KMEANS_PP_CENTERS
 
-	_, labels, centers = cv2.kmeans(data, clusters, None, criteria, iter, origin)
-
+	_, labels, centers = cv2.kmeans(data, k, None, criteria, iter, origin)
 	labels = labels.reshape(img.shape[:2])
-	labels = sort_labels(labels, centers)
 
-	return draw_clusters(labels, clusters)
+	cmap   = plt.get_cmap()
+	output = np.empty_like(img)
+
+	for i in range(k):
+		rgb = cmap(i / (k - 1))[:3]
+		output[labels == i] = [np.uint8(c * 255) for c in rgb]
+
+	return output
 
 
 A = read('Bildes/Putni.jpg')
 plot(A, 'Putni', save=False)
 
-B = gaussian_mixture(A)
-plot(B, 'Putni - Gausa adaptīvā sliekšņošana')
-B = k_means(A)
-plot(B, 'Putni - K-Means')
+B = gaussian_thresh(A)
+plot(B, 'Putni, Gausa adaptīvā sliekšņošana')
+
+B = k_means(A, k=2)
+plot(B, 'Putni, K-Means k=2')
+
+B = k_means(A, k=5)
+plot(B, 'Putni, K-Means k=5')
+
 
 A = read('Bildes/Kaķis.jpg')
 plot(A, 'Kaķis', save=False)
 
-B = gaussian_mixture(A)
-plot(B, 'Kaķis - Gausa adaptīvā sliekšņošana')
-B = k_means(A)
-plot(B, 'Kaķis - K-Means')
+B = gaussian_thresh(A)
+plot(B, 'Kaķis, Gausa adaptīvā sliekšņošana')
+
+B = k_means(A, k=2)
+plot(B, 'Kaķis, K-Means k=2')
+
+B = k_means(A, k=5)
+plot(B, 'Kaķis, K-Means k=5')
+
 
 A = read('Bildes/Dzīvnieki.jpg')
 plot(A, 'Dzīvnieki', save=False)
 
-B = gaussian_mixture(A)
-plot(B, 'Dzīvnieki - Gausa adaptīvā sliekšņošana')
-B = k_means(A)
-plot(B, 'Dzīvnieki - K-Means')
+B = gaussian_thresh(A)
+plot(B, 'Dzīvnieki, Gausa adaptīvā sliekšņošana')
+
+B = k_means(A, k=2)
+plot(B, 'Dzīvnieki, K-Means k=2')
+
+B = k_means(A, k=5)
+plot(B, 'Dzīvnieki, K-Means k=5')
 
